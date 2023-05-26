d5267358fdc4be3c4a307ef4f43a688c61f56849
Vipin Gautam <130040245+vipingautam01@users.noreply.github.com>
2023-05-26 13:05:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-function threeSum(decimalNumber) {
- if (decimalNumber === 0) {
-    return '0';
-  }
 
-  let binary = '';
-  while (decimalNumber > 0) {
-    const remainder = decimalNumber % 2;
-    binary = remainder + binary;
-    decimalNumber = Math.floor(decimalNumber / 2);
-  }
-
-  return binary;
-  
-}
-
-module.exports = threeSum;
